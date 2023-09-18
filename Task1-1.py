d5945cba49e4881f296331085a57ad87ac786318
Daria-Brattseva <brattseva.di@phystech.edu>
2023-09-18 12:42:12 -0400

    adding Task1.1

--- Task1-1.py
+++ Task1-1.py
@@ -1,5 +1,10 @@
 import numpy as np
 
-x=int(input())
-y=np.log((np.e)**(1/np.sin(x)+1)/(5/4+1/x**15))/np.log(1+x*x)
-print (y)
+x = [1, 10, 100]
+n = 3
+for i in range (n):
+    y = np.log((np.e)**(1/np.sin(x[i])+1)/(5/4+1/x[i]**15))/np.log(1+x[i]*x[i])
+
+for i in x:
+    y = np.log((np.e)**(1/np.sin(i)+1)/(5/4+1/i**15))/np.log(1+i*i)
+    print(y)
